308a35da7fdac1d4b1704dc20204fe2707d607a1
PremSai-1259 <kps483624@gmail.com>
2026-03-17 19:23:32 +0530

    fixes in honeypot

--- services/honeypot/honeypot.py
+++ services/honeypot/honeypot.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from prometheus_client import Counter, generate_latest
 import threading
 import socket
