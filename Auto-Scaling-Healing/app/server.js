9ec528cd434cad463c4a02310b639c94b2e0a4fb
Vishal Kumar Gowda <vishal.vini86@gmail.com>
2026-04-08 17:08:46 +0530

    Deleting Old Auto-Scaling-Healing directory

--- Auto-Scaling-Healing/app/server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('PodFlow Node Instance Active'));
-app.listen(3000, () => console.log('Listening on port 3000'));
